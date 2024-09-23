c4be1dd9ba3bda7c4a4351a87c6ca67638dfdb04
Danielle Soares <silvasoaresdanielle2@gmail.com>
2024-09-23 14:30:55 -0300

    user entity and resource

---
+++ src/main/java/danielle/soares/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package danielle.soares.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import danielle.soares.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "maria", "maria@gmail.co", "1111", "123");
+		return ResponseEntity.ok().body(u);
+	}
+}
