98165dd91f6b6f79f48512337344c0ec5efcbee2
Danielle Soares <silvasoaresdanielle2@gmail.com>
2024-09-23 16:10:15 -0300

    h2 database, test profile and jpa

--- src/main/java/danielle/soares/course/entities/User.java
+++ src/main/java/danielle/soares/course/entities/User.java
@@ -3,11 +3,18 @@ package danielle.soares.course.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="tb_user")
 public class User implements Serializable {
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
@@ -18,8 +25,8 @@ public class User implements Serializable {
 		
 	}
 	
-	public User(Long i, String name, String email, String phone, String password) {
-		this.id = i;
+	public User(Long id, String name, String email, String phone, String password) {
+		this.id = id;
 		this.name = name;
 		this.email = email;
 		this.phone = phone;
