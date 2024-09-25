4e0b7d0b69043c10b73ac8776f1eb2040700255d
Danielle Soares <silvasoaresdanielle2@gmail.com>
2024-09-24 22:47:29 -0300

    Order, Instant, ISO 8601

--- src/main/java/danielle/soares/course/repositories/UserRepository.java
+++ src/main/java/danielle/soares/course/repositories/UserRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import danielle.soares.course.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
-
+	
+	
 }
