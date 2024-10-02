9fd2b73fba968986f2401b8fdca1bd96571aa9a7
Danielle Soares <silvasoaresdanielle2@gmail.com>
2024-10-02 16:43:30 -0300

    Exception handling - delete

--- src/main/java/danielle/soares/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/danielle/soares/course/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import danielle.soares.course.services.exceptions.DatabaseException;
 import danielle.soares.course.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -20,4 +21,13 @@ public class ResourceExceptionHandler {
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
+	
+	@ExceptionHandler(DatabaseException.class) // indica que esse método vai interceptar qualquer exceção desse tipo em destaque.
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+		String error = "Database error";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
 }
